0ea2b794a1e438f1921e5afa5fcf5d0c0c0b5637
stuartmack <mackjohnstuart@gmail.com>
2021-01-12 21:16:42 +1100

    updated favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import favicon from "../../static/favicon.png"
 
 export default function Layout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function Layout({ children }) {
         <meta charSet="utf-8" />
         <title>strtmack</title>
         <link rel="canonical" href="https://www.strtmack.com" />
+        <link rel="icon" href={favicon} type="image/png" />
       </Helmet>
       <div className="min-h-screen font-inter bg-sand m-0 pt-32">
         {children}
